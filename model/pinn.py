2603da177b36b3c84e2189c7ba2d898152557889
LI Chenyu <153217294+chenyuli12345@users.noreply.github.com>
2024-05-04 15:52:56 +0800

    1

--- model/pinn.py
+++ model/pinn.py
@@ -7,23 +7,25 @@ import torch
 import torch.nn as nn
 
 class PINNs(nn.Module):
-    def __init__(self, in_dim, hidden_dim, out_dim, num_layer):
-        super(PINNs, self).__init__()
+    def __init__(self, in_dim, hidden_dim, out_dim, num_layer):  #用于初始化对象，接受四个参数，分别是输入数据的维度，隐藏层神经元数量，输出数据的维度，隐藏层的数量
+        super(PINNs, self).__init__() #调用父类的__init__方法进行初始化
 
-        layers = []
+        layers = [] #定义一个空列表，等价于layers = list()
         for i in range(num_layer-1):
-            if i == 0:
-                layers.append(nn.Linear(in_features=in_dim, out_features=hidden_dim))
-                layers.append(nn.Tanh())
+            if i == 0:  #对于第一层
+                layers.append(nn.Linear(in_features=in_dim, out_features=hidden_dim)) #定义了一个全连接层，输入维度为in_dim，输出维度为hidden_dim（输入层和第一个隐藏层）
+                layers.append(nn.Tanh()) 
             else:
-                layers.append(nn.Linear(in_features=hidden_dim, out_features=hidden_dim))
+                layers.append(nn.Linear(in_features=hidden_dim, out_features=hidden_dim)) #定义了全连接层，输入维度为hidden_dim，输出维度为hidden_dim（隐藏层之间）
                 layers.append(nn.Tanh())
 
-        layers.append(nn.Linear(in_features=hidden_dim, out_features=out_dim))
+        layers.append(nn.Linear(in_features=hidden_dim, out_features=out_dim)) #定义了一个全连接层，输入维度为hidden_dim，输出维度为out_dim（最后一个隐藏层和输出层）
 
+        #将所有的层组合成一个Sequential模块，即转换为一个神经网络序列模型，赋值给self.linear
         self.linear = nn.Sequential(*layers)
 
-    def forward(self, x, t):
-        src = torch.cat((x,t), dim=-1)
-        return self.linear(src)
+    #第二个方法forward，用于定义模型的前向传播过程
+    def forward(self, x, t): #接受两个参数，分别是输入数据x和t
+        src = torch.cat((x,t), dim=-1) #将输入数据x和t在最后一个维度上拼接
+        return self.linear(src) #将拼接后的数据输入到self.linear中进行前向传播
     
--- util.py
+++ util.py
@@ -1,29 +1,36 @@
 import numpy as np
 import torch.nn as nn
+#导入了copy模块。copy模块提供了复制Python对象的功能。
+#这个模块提供了浅复制（copy.copy）和深复制（copy.deepcopy）两种复制方式。#浅复制只复制对象本身，而不复制它引用的其他对象；深复制则会复制对象以及它引用的所有对象，并且新对象与原对象完全独立
 import copy
 
+#定义一个函数，用于生成一个二维网格数据。接受四个参数，分别代表x和y的范围，以及在这些范围内要生成点的数量。
 def get_data(x_range, y_range, x_num, y_num):
-    x = np.linspace(x_range[0], x_range[1], x_num)
-    t = np.linspace(y_range[0], y_range[1], y_num)
+    x = np.linspace(x_range[0], x_range[1], x_num) #在x_range[0]和x_range[1]之间生成x_num个点，构成x
+    t = np.linspace(y_range[0], y_range[1], y_num) #在y_range[0]和y_range[1]之间生成y_num个点，构成t
 
+    #生成一个二位网络，x_mesh和t_mesh是输出的二维数组，x_mesh的每一行都是x，一共len(t)行，t_mesh的每一列都是t，一共len(x)列，即二者的形状均为(len(t), len(x))
     x_mesh, t_mesh = np.meshgrid(x,t)
-    data = np.concatenate((np.expand_dims(x_mesh, -1), np.expand_dims(t_mesh, -1)), axis=-1)
+
+    #将x_mesh和t_mesh按照最后一个维度进行拼接，形状为(len(t), len(x), 2)
+    data = np.concatenate((np.expand_dims(x_mesh, -1), np.expand_dims(t_mesh, -1)), axis=-1) #这里使用了np.expand_dims函数，将x_mesh和t_mesh的维度扩展为原来的维度+1，-1表示在最后一个维度上扩展，即将一维数组扩展为二维数组(例如形状为（3，）变为（3，1）)，将二维数组扩展为三维数组
     
-    b_left = data[0,:,:] 
-    b_right = data[-1,:,:]
-    b_upper = data[:,-1,:]
-    b_lower = data[:,0,:]
-    res = data.reshape(-1,2)
+    b_left = data[0,:,:]  #取出data的第一行，即左边界，形状为(len(x), 2)
+    b_right = data[-1,:,:] #取出data的最后一行，即右边界，形状为(len(x), 2)
+    b_upper = data[:,-1,:] #取出data的最后一列，即上边界，形状为(len(t), 2)
+    b_lower = data[:,0,:] #取出data的第一列，即下边界，形状为(len(t), 2)
+    res = data.reshape(-1,2) #将data的形状变为(len(t)*len(x), 2)，即(N, 2)
 
-    return res, b_left, b_right, b_upper, b_lower
+    return res, b_left, b_right, b_upper, b_lower #返回生成的数据，以及四个边界数据
 
-
+#定义一个函数，用于计算模型的参数数量。传入参数为模型，最后返回模型的参数数量？？？
 def get_n_params(model):
-    pp=0
-    for p in list(model.parameters()):
-        nn=1
-        for s in list(p.size()):
-            nn = nn*s
+    pp=0 #定义一个变量pp，用于存储参数数量
+    #遍历模型的所有参数
+    for p in list(model.parameters()): #model.parameters()返回模型的所有参数，list将其转换为列表，p代表每一个参数
+        nn=1 #定义一个变量nn，用于存储该参数数量
+        for s in list(p.size()):  #p.size()是一个元组，包含了参数p在每个维度上的大小，list将其转换为列表，s代表每一个维度的大小
+            nn = nn*s #将每个维度的大小相乘，得到该参数的数量。当遍历完p的所有维度后，nn即为该参数的数量
         pp += nn
     return pp
 
@@ -35,10 +42,10 @@ def make_time_sequence(src, num_step=5, step=1e-4):
         src[:,i,-1] += step*i
     return src
 
-
-def get_clones(module, N):
+#作用是创建N个module的深度复制，并将它们作为一个nn.ModuleList返回
+def get_clones(module, N): #接受两个参数，module是一个pytorch模块，N是一个证书
     return nn.ModuleList([copy.deepcopy(module) for i in range(N)])
-
+#函数主体是一个列表推导式，它会生成一个列表，列表中包含N个module的深度复制。而nn.ModuleList是一个包含各种模块的简单列表，与普通列表相比，其重要特性是会作为模块属性时，会自动注册其包含的模块
 
 def get_data_3d(x_range, y_range, t_range, x_num, y_num, t_num):
     step_x = (x_range[1] - x_range[0]) / float(x_num-1)
